a8501f9db4d7e6fa3c7d3a2228aa03be98d96b1d
Dan2001afk <camolo.777@gmail.com>
2023-06-27 22:13:35 -0500

    archivo con pruebas metodo insertar

--- js/listar.js
+++ js/listar.js
@@ -23,3 +23,32 @@ $(document).ready(function() {
         });
     });
 });
+
+
+$('#Agregar').on('click',function(){
+
+    let datos={
+        equ_id:$('#equ_id').val(),
+        equi_tipo:$('#equi_tipo').val(),
+        equi_modelo:$('#equi_modelo').val(),
+        equi_color:$('#equi_color').val(),
+        equi_serial:$('#equi_serial').val(),    
+        equi_estado:$('#equi_estado').val(),
+        equi_especialidad:$('#equi_especialidad').val(),
+
+
+    }
+    let datosenvio=JSON.stringify(datos)
+    console.log(datos)
+    console.log(datosenvio)
+    $.ajax({
+        url: "http://localhost:8080/InsertarEquipo/",
+        type: "POST",
+        data: datosenvio,
+        contentType: "application/JSON",
+        datatype: JSON,
+        success: function(respuesta){
+            alert(respuesta)
+        }
+    })
+})
